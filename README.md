5a86cd02be2e67e3efe549a8383065e65a6ac21b
Loligplayer33 <dhaase033@gmail.com>
2020-07-23 10:55:26 +0200

    basic project structure implemented

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-react boilerplate created with create-react-app without all the files no one ever needs.
+Movie finder web app.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import LandingPage from './Pages/LandingPage';
+import { Router } from '@reach/router';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
-  return <div>Hi there</div>;
+  return (
+    <>
+      <Navbar />
+      <Router>
+        <LandingPage path="/" />
+      </Router>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
